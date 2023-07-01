c3edbb1cdc581376cd580eaa7fb8f10df86e6355
LighteningBolt13 <joywinpinto1@gmail.com>
2023-07-01 16:11:35 +0530

    Deployment

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -7,10 +7,7 @@ const notAvailable = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.status.Code === 200 ? 500 : res.status.Code;
   res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
+  
 };
 
 module.exports = { notAvailable, errorHandler };
